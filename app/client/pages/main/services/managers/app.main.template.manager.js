bb7de6887e08be243fa158d6519bfac07b4babb3
sunghohw <sunghohw@andrew.cmu.edu>
2018-07-26 17:55:14 +0900

    add create in template manager

--- app/client/pages/main/services/managers/app.main.template.manager.js
+++ app/client/pages/main/services/managers/app.main.template.manager.js
@@ -1,7 +1,11 @@
 export default function templateManager (Templates, metaManager, dialogHandler, statusHandler) {
     'ngInject';
 
+    this.findById = findById;
     this.findAll = findAll;
+    this.create = create;
+    this.update = update;
+    this.remove = remove;
 
     function findAll (data, callback) {
 
@@ -16,6 +20,23 @@ export default function templateManager (Templates, metaManager, dialogHandler, 
             
             Templates.query(data, function (data) {
                 callback(200, data);
+            }, function (data) {
+                statusHandler.active(data, callback);
+            });
+        });
+    }
+    function create(data,callback) {
+        var body = {};
+        if(data.title !== undefined) body.title = data.title;
+        if(data.body !== undefined) body.body = data.body;
+
+        dialogHandler.validator(body, [
+            'title',
+            'body'
+        ], null, null, function (data) {
+            var templates = new Templates(data);
+            templates.$save(data, function (data) {
+                callback(201, data);
             }, function (data) {
                 statusHandler.active(data, callback);
             });
